4a8aaf75d6b1883b9389ffa7ac89e998da1f5f85
curso09 <curso09@nomail.com>
2024-08-23 09:35:27 +0200

    Fix PersonaRepository

--- src/main/java/es/cic/curso/vuerest/repository/PersonaRepository.java
+++ src/main/java/es/cic/curso/vuerest/repository/PersonaRepository.java
@@ -3,6 +3,8 @@ package es.cic.curso.vuerest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import es.cic.curso.vuerest.model.Persona;
+
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
