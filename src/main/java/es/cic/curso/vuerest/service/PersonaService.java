793569ffdb262d1495bb91765692723bc0b19d57
curso09 <curso09@nomail.com>
2024-08-23 09:44:06 +0200

    Service Persona

---
+++ src/main/java/es/cic/curso/vuerest/service/PersonaService.java
@@ -0,0 +1,27 @@
+package es.cic.curso.vuerest.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.cic.curso.vuerest.model.Persona;
+import es.cic.curso.vuerest.repository.PersonaRepository;
+
+@Service
+public class PersonaService {
+
+    @Autowired
+    PersonaRepository personaRepository;
+
+    public Optional<Persona> findById(Long id) {
+        return personaRepository.findById(id);
+    }
+
+    public long crearPersona(String nombre, String apellidos, int annoNacimiento) {
+        Persona persona = new Persona(nombre, apellidos, annoNacimiento);
+        personaRepository.save(persona);
+        return persona.getId();
+    }
+
+}
