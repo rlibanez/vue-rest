881b9da74b8fe84a323922d6d84a23f7396f1540
curso09 <curso09@nomail.com>
2024-08-23 09:51:47 +0200

    Controller Persona

---
+++ src/main/java/es/cic/curso/vuerest/controller/PersonaController.java
@@ -0,0 +1,23 @@
+package es.cic.curso.vuerest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.cic.curso.vuerest.model.Persona;
+import es.cic.curso.vuerest.service.PersonaService;
+
+@RestController
+@RequestMapping("/api/persona")
+public class PersonaController {
+
+    @Autowired
+    private PersonaService personaService;
+
+    @GetMapping("/{id}")
+    public Persona getPersona(@PathVariable Long id) {
+        return personaService.findById(id).orElse(null);
+    }
+}
