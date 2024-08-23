313f1b6e69be68b3b121f6ead2c63e2add416404
curso09 <curso09@nomail.com>
2024-08-23 09:30:16 +0200

    Model Persona

---
+++ src/main/java/es/cic/curso/vuerest/model/Persona.java
@@ -0,0 +1,100 @@
+package es.cic.curso.vuerest.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellidos;
+    private int annoNacimiento;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellidos, int annoNacimiento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.annoNacimiento = annoNacimiento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getAnnoNacimiento() {
+        return annoNacimiento;
+    }
+
+    public void setAnnoNacimiento(int annoNacimiento) {
+        this.annoNacimiento = annoNacimiento;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+        result = prime * result + annoNacimiento;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona other = (Persona) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (apellidos == null) {
+            if (other.apellidos != null)
+                return false;
+        } else if (!apellidos.equals(other.apellidos))
+            return false;
+        if (annoNacimiento != other.annoNacimiento)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", annoNacimiento="
+                + annoNacimiento + "]";
+    }
+}
