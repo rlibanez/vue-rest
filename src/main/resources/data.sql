37eaa71c4489a709a0b8b11752ee8a26bea846b5
curso09 <curso09@nomail.com>
2024-08-23 09:38:40 +0200

    SQL por defecto para cargar datos a la DB H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO persona (nombre, apellidos, annoNacimiento) VALUES ('Juan', 'Pérez', 1980);
+INSERT INTO persona (nombre, apellidos, annoNacimiento) VALUES ('María', 'González', 1990);
+INSERT INTO persona (nombre, apellidos, annoNacimiento) VALUES ('Carlos', 'Rodríguez', 1975);
+INSERT INTO persona (nombre, apellidos, annoNacimiento) VALUES ('Pepe', 'Pérez', 1989);
+INSERT INTO persona (nombre, apellidos, annoNacimiento) VALUES ('Rubén', 'González', 2011);
+INSERT INTO persona (nombre, apellidos, annoNacimiento) VALUES ('Ana', 'Rodríguez', 1979);
