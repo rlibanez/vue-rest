d7aa5279ada095a2526bce4abb5d277b9791ef08
curso09 <curso09@nomail.com>
2024-08-29 10:42:33 +0200

    Añadir métodos faltantes LibroController

--- src/main/java/es/cic/curso/vuerest/controller/LibroController.java
+++ src/main/java/es/cic/curso/vuerest/controller/LibroController.java
@@ -3,8 +3,12 @@ package es.cic.curso.vuerest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +30,27 @@ public class LibroController {
     @GetMapping
     public List<Libro> getAllLibros() {
         return libroService.findAll();
+    }
+
+    @PostMapping
+    public Libro createLibro(@RequestBody Libro libro) {
+        return libroService.save(libro);
+    }
+
+    @PutMapping
+    public Libro updateLibro(@RequestBody Libro libroDetails) {
+        Long id = libroDetails.getId();
+        return libroService.findById(id).map(libro -> {
+            libro.setTitulo(libroDetails.getTitulo());
+            libro.setIsbn(libroDetails.getIsbn());
+            libro.setAnnoPublicacion(libroDetails.getAnnoPublicacion());
+            return libroService.save(libro);
+        }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLibro(@PathVariable Long id) {
+        libroService.deleteById(id);
     }
 
 }
